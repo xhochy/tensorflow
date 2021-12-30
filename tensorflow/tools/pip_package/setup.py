5eefe47f14ec189181974554fc58f4bbe9ae3c9d
Uwe L. Korn <uwe.korn@quantco.com>
2021-12-30 06:57:09 +0200

    loosen requirements
    * Remove unused libclang requirement
    * Remove unecessary tensorflow-io-gcs-filesystem
    * unpin numpy
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    Co-authored-by: ngam

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -88,7 +88,6 @@ REQUIRED_PACKAGES = [
     'gast >=0.2.1,!=0.5.0,!=0.5.1,!=0.5.2',
     'google_pasta >= 0.1.1',
     'h5py >= 2.9.0',
-    'libclang >= 13.0.0',
     'ml_dtypes ~= 0.2.0',
     'numpy >= 1.23.5, < 2.0.0',
     'opt_einsum >= 2.3.2',
@@ -102,7 +101,6 @@ REQUIRED_PACKAGES = [
     'termcolor >= 1.1.0',
     'typing_extensions >= 3.6.6',
     'wrapt >= 1.11.0, < 1.15',
-    'tensorflow-io-gcs-filesystem >= 0.23.1',
     # grpcio does not build correctly on big-endian machines due to lack of
     # BoringSSL support.
     # See https://github.com/tensorflow/tensorflow/issues/17882.
